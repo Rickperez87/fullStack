71ce8d5bb085910da5e6ab636b5ea1c3ac19c5d3
Rick Perez <Rickperez87@gmail.com>
2020-08-21 08:47:32 -0400

    configureing packages for heroku deployment

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const app = express();
 const router = require("./router");
 const cors = require("cors");
 const adminMain = require("./routes/adminMain");
-const morgan = require("morgan");
 const helmet = require("helmet");
 const compression = require("compression");
 
 app.use(helmet());
-app.use(morgan("short"));
 app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
