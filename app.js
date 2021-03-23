b74de88a170fd33951407a6e4e1046d2d0a3b257
Rick Perez <Rickperez87@gmail.com>
2021-03-23 15:57:43 -0400

    update static serve path

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const router = require("./router");
 const cors = require("cors");
@@ -9,7 +10,7 @@ const compression = require("compression");
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/", adminMain);
 app.use("/api/data", router);
 
